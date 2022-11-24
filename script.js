d7ac7eec9e8bb82758ab386e441cfe32bce91dc6
chowweijun <chowjor@gmail.com>
2022-11-24 17:01:56 +0800

    finished reviews

--- script.js
+++ script.js
@@ -3,12 +3,14 @@ const navLinks = document.querySelector('.nav-links');
 const allNavLinks = document.querySelectorAll('.nav-link');
 const navLogo = document.querySelector('.nav-logo');
 const navForm = document.querySelector('.nav-form');
+
+
 /* nav links opacity */
 
 
 // mouseover
 navLinks.addEventListener('mouseover',(e) => {
-    if(e.target.className === 'nav-link'){
+    if(e.target.classList.contains('nav-link')){
         let hovered = e.target;   
         allNavLinks.forEach(link => {
             if(link != hovered){
@@ -39,7 +41,85 @@ navLinks.addEventListener('click', (e) => {
     }
 })
 
+/* open form/overlay */
 
+const headerBtn = document.querySelector('.header-btn');
+const modal = document.querySelector('.form-particulars');
+const overlay = document.querySelector('.overlay');
 
+headerBtn.addEventListener('click',() => {
+    modal.classList.remove('hidden')
+    overlay.classList.remove('hidden')
+})
 
+overlay.addEventListener('click',()=>{
+    modal.classList.add('hidden')
+    overlay.classList.add('hidden')
+})
+
+/* make hidden content appear as we scroll */
+/* INTL */
+
+const allContent = document.querySelectorAll('.contents');
+
+const contentCallback = function(entries,observer){
+    entries.forEach(entry => {
+        if(!entry.isIntersecting) return;
+        entry.target.classList.remove('blur')
+    })
+}
+
+const contentOptions = {
+    root:null,
+    threshold:0.5
+}
+
+const contentObserver = new IntersectionObserver(contentCallback,contentOptions)
+allContent.forEach(content => contentObserver.observe(content))
+
+/* reviews */
+let array = []
+const textBox = document.querySelector('#reviews-text');
+const allReviews = document.querySelector('.allreviews');
+const reviewList = document.querySelectorAll('.review');
+const reviewBtn = document.querySelector('.reviews-btn');
+
+const setLocalStorage = function(){
+    localStorage.setItem('reviews',JSON.stringify(array,["textContent"]))
+}
+
+const getLocalStorage = function(){
+    const data = JSON.parse(localStorage.getItem('reviews'))
+    array = data
+    data.forEach(string => newReview(string.textContent))
+    
+    
+}
+
+const newReview = function(string){
+    const html = `<div class="review">${string}</div>`
+    allReviews.insertAdjacentHTML("beforeend",html)
+}
+
+textBox.addEventListener('keydown',(e)=>{
+    let string = textBox.value;
+    if(e.keyCode == 13){
+        e.preventDefault()
+        if(string === "") return;
+        newReview(string)
+        textBox.value = ""
+        array.push(document.querySelector('.review:last-child'))
+        setLocalStorage()
+    }
+})
+
+reviewBtn.addEventListener('click',()=> {
+    localStorage.removeItem('reviews')
+    array = []
+    setLocalStorage()
+    location.reload()
+})
+
+getLocalStorage()
+setLocalStorage()
 
